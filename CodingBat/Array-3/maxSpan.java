47ae3b9e509411f04cf8062bb30a92c941f1cb49
Scott Brettle <srbrettle@gmail.com>
2019-01-13 19:56:57 +0000

    Create maxSpan.java

---
+++ CodingBat/Array-3/maxSpan.java
@@ -0,0 +1,21 @@
+public int maxSpan(int[] nums) {
+  int maxSpan = 0;
+  // Enumerate through array
+  for (int i = 0; i < nums.length; i++) {
+    int span = 0;
+    int value = nums[i];
+    // Enumerate through remainder of array
+    for (int j = i; j < nums.length; j++) {
+      ++span;
+      if (nums[j] == value) {
+        // Current int matches with target int
+        if (span > maxSpan) {
+          // Update max span if current span is higher
+          maxSpan = span;
+        }
+      }
+    }  
+  }
+  
+  return maxSpan;
+}
