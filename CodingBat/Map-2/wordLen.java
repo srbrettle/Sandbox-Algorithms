329a6df5998fe839679188dcee5d2da8331ddaff
Scott Brettle <srbrettle@gmail.com>
2019-01-17 19:17:14 +0000

    Create wordLen.java

---
+++ CodingBat/Map-2/wordLen.java
@@ -0,0 +1,11 @@
+// https://codingbat.com/prob/p125327
+
+public Map<String, Integer> wordLen(String[] strings) {
+  Map<String, Integer> map = new HashMap<String, Integer>();
+  for(String str : strings) {
+    if (!map.containsKey(str)) {
+      map.put(str, str.length());
+    }
+  }
+  return map;
+}
