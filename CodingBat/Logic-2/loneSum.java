2aef63fd734cf49d2683577c6cb420b6ae03203f
Scott Brettle <srbrettle@gmail.com>
2019-01-18 18:52:28 +0000

    Create loneSum.java

---
+++ CodingBat/Logic-2/loneSum.java
@@ -0,0 +1,15 @@
+// https://codingbat.com/prob/p148972
+
+public int loneSum(int a, int b, int c) {
+  int sum = 0;
+  if ((a != b) && (a != c)) {
+    sum += a;
+  }
+  if ((b != a) && (b != c)) {
+    sum += b;
+  }
+  if ((c != a) && (c != b)) {
+    sum += c;
+  }
+  return sum; 
+}
