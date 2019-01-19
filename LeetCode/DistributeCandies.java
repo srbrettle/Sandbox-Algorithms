9353d5a7b4074fb22f3852944a0f0a7fa7ba4fb7
Scott Brettle <srbrettle@gmail.com>
2019-01-19 16:42:04 +0000

    Update DistributeCandies.java

--- LeetCode/DistributeCandies.java
+++ LeetCode/DistributeCandies.java
@@ -1,21 +1,22 @@
 // https://leetcode.com/problems/distribute-candies/submissions/
-// Details - Runtime: 74 ms, faster than 88.42% of Java online submissions for Distribute Candies.
+// Details - Runtime: 53 ms, faster than 98.00% of Java online submissions for Distribute Candies.
 
 class Solution {
     public int distributeCandies(int[] candies) {
-        HashSet<Integer> set = new HashSet<Integer>();
-        // Create set of unique candies
-        for (int candy : candies) {
-            set.add(candy);
-        }
+        int halfAllCandies = candies.length/2;
+        HashSet<Integer> uniqueCandies = new HashSet<Integer>(halfAllCandies);
         
-        // If more unique candies that half the total count
-        // then return half the total count
-        if (set.size() > (candies.length / 2)) {
-            return candies.length/2;
-        }
+        // Create set of unique candies
+        for (int candy : candies) {            
+            if (uniqueCandies.size() == halfAllCandies) {
+                // If more unique candies that half the total count
+                // then return half the total count                
+                return halfAllCandies;               
+            }
+            uniqueCandies.add(candy);
+        }    
         
         // Return the number of unique candies
-        return set.size();
+        return uniqueCandies.size();
     }
 }
