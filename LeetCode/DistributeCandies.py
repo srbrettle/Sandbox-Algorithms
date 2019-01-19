eeb022929e4f45393c09685a814044d4480ef4f9
Scott Brettle <srbrettle@gmail.com>
2019-01-19 17:10:07 +0000

    Create DistributeCandies.py

---
+++ LeetCode/DistributeCandies.py
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/distribute-candies
+// Details - Runtime: 140 ms, faster than 82.35% of Python online submissions for Distribute Candies.
+
+class Solution:
+    def distributeCandies(self, candies):        
+        halfAllCandies = int(len(candies)/2)
+        uniqueCandies = len(set(candies))
+            
+        return min(halfAllCandies, uniqueCandies)                                    
