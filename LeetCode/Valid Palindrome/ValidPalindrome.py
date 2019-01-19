9589b8276847545ccbf581c188a89d3914a92213
Scott Brettle <srbrettle@gmail.com>
2019-01-19 18:47:16 +0000

    Update ValidPalindrome.py

--- LeetCode/Valid Palindrome/ValidPalindrome.py
+++ LeetCode/Valid Palindrome/ValidPalindrome.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/valid-palindrome
-# Details - Runtime: 80 ms, faster than 96.90% of Python3 online submissions for Valid Palindrome.
+# Python Details - Runtime: 48 ms, faster than 98.95% of Python online submissions for Valid Palindrome.
+# Python3 Details - Runtime: 80 ms, faster than 96.90% of Python3 online submissions for Valid Palindrome.
 
 class Solution:
     def isPalindrome(self, s):
