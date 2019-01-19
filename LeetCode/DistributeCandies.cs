d727aac7ad60929df3309422aa3beca25bf4f5e6
Scott Brettle <srbrettle@gmail.com>
2019-01-19 16:46:17 +0000

    Update DistributeCandies.cs

--- LeetCode/DistributeCandies.cs
+++ LeetCode/DistributeCandies.cs
@@ -1,20 +1,23 @@
 // https://leetcode.com/problems/distribute-candies/
 
+// Details - Runtime: 316 ms, faster than 97.67% of C# online submissions for Distribute Candies.
+
 public class Solution {
     public int DistributeCandies(int[] candies) {
-        var set = new HashSet<int>();
+        int halfAllCandies = candies.Length / 2;
+        HashSet<int> uniqueCandies = new HashSet<int>(halfAllCandies);
         // Create set of unique candies
         foreach (int candy in candies) {
-            set.Add(candy);
-        }
-        
-        // If more unique candies that half the total count
-        // then return half the total count
-        if (set.Count > (candies.Count() / 2)) {
-            return candies.Count()/2;
-        }
+            uniqueCandies.Add(candy);
+            
+            // If same/more unique candies that half the total count
+            // then return half the total count
+            if (uniqueCandies.Count == halfAllCandies) {
+                return halfAllCandies;
+            }            
+        }                
         
         // Return the number of unique candies
-        return set.Count;
+        return uniqueCandies.Count;
     }
 }
