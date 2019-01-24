434ea17cbc71555cdb22cae2d79e3524b54fd491
Scott Brettle <srbrettle@gmail.com>
2019-01-24 21:55:50 +0000

    Create Algorithms-C#-Easy.cs

---
+++ HackerRank/Algorithms-C#-Easy.cs
@@ -0,0 +1,17 @@
+// https://www.hackerrank.com/challenges/compare-the-triplets/
+// Complete the compareTriplets function below.
+    static List<int> compareTriplets(List<int> a, List<int> b) {
+        List<int> scores = new List<int>() { 0, 0 };        
+        for (int i = 0; i < a.Count; i++)
+        {
+            if (a[i] > b[i]) {
+                ++scores[0];
+            }
+            else if (a[i] < b[i]) {
+                ++scores[1];
+            }
+        }
+        return scores;
+    }
+    
+    
