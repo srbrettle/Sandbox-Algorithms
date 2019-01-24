1e6057e29ec22c05fc55f4bb73a3eb1c22d851ea
Scott Brettle <srbrettle@gmail.com>
2019-01-24 22:02:28 +0000

    Create DataStructures-C#-Easy.cs

---
+++ HackerRank/DataStructures-C#-Easy.cs
@@ -0,0 +1,5 @@
+// https://www.hackerrank.com/challenges/arrays-ds
+// Complete the reverseArray function below.
+    static int[] reverseArray(int[] a) {
+        return a.Reverse().ToArray();
+    }
