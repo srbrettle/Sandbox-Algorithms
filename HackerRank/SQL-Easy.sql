4aa70de324ef23a1a1e2ba857274ed0cd74b029b
Scott Brettle <srbrettle@gmail.com>
2019-01-25 21:30:28 +0000

    Update SQL-Easy.sql

--- HackerRank/SQL-Easy.sql
+++ HackerRank/SQL-Easy.sql
@@ -1,22 +1,22 @@
-# Revising the Select Query
-# https://www.hackerrank.com/challenges/revising-the-select-query/problem
+-- Revising the Select Query
+-- https://www.hackerrank.com/challenges/revising-the-select-query/problem
 SELECT ID, NAME, COUNTRYCODE, DISTRICT, POPULATION
 FROM CITY
 WHERE COUNTRYCODE = 'USA' AND POPULATION > 100000;
 
-# Revising the Select Query II
-# https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
+-- Revising the Select Query II
+-- https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
 SELECT NAME
 FROM CITY
 WHERE COUNTRYCODE = 'USA' AND POPULATION > 120000;
 
-# Select All
-# https://www.hackerrank.com/challenges/select-all-sql/problem
+-- Select All
+-- https://www.hackerrank.com/challenges/select-all-sql/problem
 SELECT ID, NAME, COUNTRYCODE, DISTRICT, POPULATION
 FROM CITY;
 
-# Select by ID
-# https://www.hackerrank.com/challenges/select-by-id/problem
+-- Select by ID
+-- https://www.hackerrank.com/challenges/select-by-id/problem
 SELECT ID, NAME, COUNTRYCODE, DISTRICT, POPULATION
 FROM CITY
 WHERE ID = 1661;
