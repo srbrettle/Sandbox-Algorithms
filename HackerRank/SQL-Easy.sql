0a861d11ae9a5ea3640c3e45fbc36b36d3ebfa0e
Scott Brettle <srbrettle@gmail.com>
2019-01-26 10:36:07 +0000

    Update SQL-Easy.sql

--- HackerRank/SQL-Easy.sql
+++ HackerRank/SQL-Easy.sql
@@ -69,3 +69,109 @@ FROM
     ORDER BY length DESC, CITY
 ) 
 WHERE rownum = 1;
+
+-- Weather Observation Station 6 
+-- https://www.hackerrank.com/challenges/weather-observation-station-6/problem
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY LIKE 'A%' 
+OR CITY LIKE 'E%' 
+OR CITY LIKE 'I%' 
+OR CITY LIKE 'O%' 
+OR CITY LIKE 'U%';
+
+-- Weather Observation Station 7
+-- https://www.hackerrank.com/challenges/weather-observation-station-7/problem
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY LIKE '%a' 
+OR CITY LIKE '%e' 
+OR CITY LIKE '%i' 
+OR CITY LIKE '%o' 
+OR CITY LIKE '%u';
+
+-- Weather Observation Station 8
+-- https://www.hackerrank.com/challenges/weather-observation-station-8/problem
+SELECT DISTINCT CITY
+FROM STATION
+WHERE 
+(
+    CITY LIKE '%a' 
+    OR CITY LIKE '%e' 
+    OR CITY LIKE '%i' 
+    OR CITY LIKE '%o' 
+    OR CITY LIKE '%u'
+)
+AND 
+(
+    CITY LIKE 'A%' 
+    OR CITY LIKE 'E%' 
+    OR CITY LIKE 'I%' 
+    OR CITY LIKE 'O%' 
+    OR CITY LIKE 'U%'
+);
+
+-- Weather Observation Station 9
+-- https://www.hackerrank.com/challenges/weather-observation-station-9/problem
+SELECT DISTINCT CITY
+FROM STATION
+WHERE NOT CITY LIKE 'A%' 
+AND NOT CITY LIKE 'E%' 
+AND NOT CITY LIKE 'I%' 
+AND NOT CITY LIKE 'O%' 
+AND NOT CITY LIKE 'U%';
+
+-- Weather Observation Station 10
+-- https://www.hackerrank.com/challenges/weather-observation-station-10/problem
+SELECT DISTINCT CITY
+FROM STATION
+WHERE NOT CITY LIKE '%a' 
+AND NOT CITY LIKE '%e' 
+AND NOT CITY LIKE '%i' 
+AND NOT CITY LIKE '%o' 
+AND NOT CITY LIKE '%u';
+
+-- Weather Observation Station 11
+-- https://www.hackerrank.com/challenges/weather-observation-station-11/problem
+SELECT DISTINCT CITY
+FROM STATION
+WHERE NOT
+(
+    CITY LIKE '%a' 
+    OR CITY LIKE '%e' 
+    OR CITY LIKE '%i' 
+    OR CITY LIKE '%o' 
+    OR CITY LIKE '%u'
+)
+OR NOT 
+(
+    CITY LIKE 'A%' 
+    OR CITY LIKE 'E%' 
+    OR CITY LIKE 'I%' 
+    OR CITY LIKE 'O%' 
+    OR CITY LIKE 'U%'
+);
+
+-- Weather Observation Station 12
+-- https://www.hackerrank.com/challenges/weather-observation-station-12/problem
+SELECT DISTINCT CITY
+FROM STATION
+WHERE NOT
+(
+    CITY LIKE '%a' 
+    OR CITY LIKE '%e' 
+    OR CITY LIKE '%i' 
+    OR CITY LIKE '%o' 
+    OR CITY LIKE '%u'
+)
+AND NOT 
+(
+    CITY LIKE 'A%' 
+    OR CITY LIKE 'E%' 
+    OR CITY LIKE 'I%' 
+    OR CITY LIKE 'O%' 
+    OR CITY LIKE 'U%'
+);
+
+-- 
+-- 
