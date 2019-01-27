a3af8b1e0c0264051a9fe49e0759869434fa8170
Scott Brettle <srbrettle@gmail.com>
2019-01-27 19:31:12 +0000

    Refactored to use statistics library

--- HackerRank/Tutorials/10 Days of Statistics/Day 1: Quartiles.py
+++ HackerRank/Tutorials/10 Days of Statistics/Day 1: Quartiles.py
@@ -1,35 +1,23 @@
-import math
+import statistics as st
 
 # get inputs
 size = int(input())
 values = list(map(int, input().split(" ")))
 values.sort()
 
-if (size % 2 == 0):  
-    # size is even      
-    second_quartile = int((values[int(size/2-1)]+values[int(size/2)])/2)        
-    if ((size/2) % 2 == 0):
-        # half size is even
-        first_quartile = int((values[int(math.floor(size/4)-1)]+values[int(math.floor(size/4))])/2)         
-        third_quartile = int((values[int(math.floor(3*size/4)-1)]+values[int(math.floor(3*size/4))])/2)        
-    else:
-        # half size is odd
-        first_quartile = values[int(math.floor(size/4))]
-        third_quartile = values[int(math.floor(3*size/4))]
-        
-else:    
+# Calculate median and ranges
+median = st.median(values)
+if (size % 2 == 0):
+    # size is even
+    first_quartile = st.median(values[:size//2])
+    third_quartile = st.median(values[size//2:])    
+else:
     # size is odd
-    second_quartile = values[int(size/2-0.5)]  
-    if ((size-1) % 2 == 0):
-        # size without median is even
-        first_quartile = int((values[int(math.floor(size/4)-1)]+values[int(math.floor(size/4))])/2) 
-        third_quartile = int((values[int(math.floor(3*size/4))]+values[int(math.floor(3*size/4)+1)])/2)        
-    else:        
-        # size without median is odd
-        first_quartile = values[int((size-1)/3)-1]
-        third_quartile = values[int(2*(size-1)/3+1)]
+    first_quartile = st.median(values[:size//2])
+    third_quartile = st.median(values[size//2+1:])
 
 # print outputs
-print(first_quartile)
-print(second_quartile)
-print(third_quartile)
+print(int(first_quartile))
+print(int(median))
+print(int(third_quartile))
+
