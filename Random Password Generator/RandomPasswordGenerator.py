37af3c3b76243f56b55e75687a9a99e012710099
Scott Brettle <srbrettle@gmail.com>
2019-02-02 18:01:42 +0000

    Update RandomPasswordGenerator.py

--- Random Password Generator/RandomPasswordGenerator.py
+++ Random Password Generator/RandomPasswordGenerator.py
@@ -4,27 +4,22 @@ import string
 vowels = ['a','e','i','o','u']
 consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','x','y','z']
 
-def generate_cvc_password(splitter):
+
+def generate_cvc_password(splitter=''):
     return generate_cvc() + splitter + generate_cvc() + splitter + generate_cvc()
 
 def generate_cvc():
     return random.choice(consonants) + random.choice(vowels) + random.choice(consonants)
     
-def random_password(length):
+def random_password(length, useSpecials=True):
     password = ""
     options = string.punctuation + string.digits + string.ascii_letters
+    if useSpecials:
+        options += string.punctuation
     for n in range(0,length):
         password += random.choice(options)
     return password
     
-def random_password_without_specials(length):
-    password = ""
-    options = string.digits + string.ascii_letters
-    for n in range(0,length):
-        password += random.choice(options)
-    return password
-
-# Print examples
 print('Random Characters:')
 print (random_password(18))
 print (random_password(18))
@@ -32,11 +27,11 @@ print (random_password(18))
 print (random_password(18))
 print (random_password(18) + "\r\n")
 print('Random Characters without Special Characters:')
-print (random_password_without_specials(18))
-print (random_password_without_specials(18))
-print (random_password_without_specials(18))
-print (random_password_without_specials(18))
-print (random_password_without_specials(18) + "\r\n")
+print (random_password(18, False))
+print (random_password(18, False))
+print (random_password(18, False))
+print (random_password(18, False))
+print (random_password(18, False) + "\r\n")
 print("Random CVC Type:")
 print(generate_cvc_password('-'))
 print(generate_cvc_password('-'))
@@ -44,8 +39,8 @@ print(generate_cvc_password('-'))
 print(generate_cvc_password('-'))
 print(generate_cvc_password('-') + "\r\n")
 print("Random CVC Type Without Splitter:")
-print(generate_cvc_password(''))
-print(generate_cvc_password(''))
-print(generate_cvc_password(''))
-print(generate_cvc_password(''))
-print(generate_cvc_password('') + "\r\n")
+print(generate_cvc_password())
+print(generate_cvc_password())
+print(generate_cvc_password())
+print(generate_cvc_password())
+print(generate_cvc_password() + "\r\n")
