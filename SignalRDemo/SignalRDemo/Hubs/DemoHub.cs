d717ef854b97df9182ce945553a94e2e774e2a7e
Scott Brettle <srbrettle@gmail.com>
2019-03-20 21:26:05 +0000

    Updated to demo manipulation of data and response.

--- SignalRDemo/SignalRDemo/Hubs/DemoHub.cs
+++ SignalRDemo/SignalRDemo/Hubs/DemoHub.cs
@@ -5,9 +5,17 @@ namespace SignalRDemo.Hubs
 {
     public class DemoHub : Hub
     {
+        public async Task Adder(int a, int b, int c)
+        {
+            int sum = a + b + c;
+            await Clients.All.SendAsync("ReceiveMessage", sum);
+        }
+
+        /*
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
+        */
     }
 }
--- SignalRDemo/SignalRDemo/wwwroot/js/demo.js
+++ SignalRDemo/SignalRDemo/wwwroot/js/demo.js
@@ -5,11 +5,9 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/demoHub").build();
 //Disable send button until connection is established
 document.getElementById("sendButton").disabled = true;
 
-connection.on("ReceiveMessage", function (user, message) {
-    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    var encodedMsg = user + " says " + msg;
+connection.on("ReceiveMessage", function (sum) {
     var li = document.createElement("li");
-    li.textContent = encodedMsg;
+    li.textContent = sum;
     document.getElementById("messagesList").appendChild(li);
 });
 
@@ -20,9 +18,10 @@ connection.start().then(function () {
 });
 
 document.getElementById("sendButton").addEventListener("click", function (event) {
-    var user = document.getElementById("userInput").value;
-    var message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessage", user, message).catch(function (err) {
+    var intA = document.getElementById("inputIntA").value;
+    var intB = document.getElementById("inputIntB").value;
+    var intC = document.getElementById("inputIntC").value;
+    connection.invoke("Adder", intA, intB, intC).catch(function (err) {
         return console.error(err.toString());
     });
     event.preventDefault();
